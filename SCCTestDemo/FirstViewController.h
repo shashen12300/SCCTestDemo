c0d8a18e960c6d5316c1e52ad04a33eaf0b72ca2
songchongchong <songcc@jingzhaokeji.com>
2015-12-30 17:27:17 +0800

    second commit

---
+++ SCCTestDemo/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  SCCTestDemo
+//
+//  Created by mijibao on 15/12/30.
+//  Copyright © 2015年 songcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : BaseViewController
+
+@end
---
+++ SCCTestDemo/FirstViewController.m
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.m
+//  SCCTestDemo
+//
+//  Created by mijibao on 15/12/30.
+//  Copyright © 2015年 songcc. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@implementation FirstViewController
+
+@end
