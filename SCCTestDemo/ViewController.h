712a762b7d9e9cddc6b7a5939e0ba877b6588096
songchongchong <songcc@jingzhaokeji.com>
2015-12-30 16:34:13 +0800

    Initial Commit

---
+++ SCCTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCCTestDemo
+//
+//  Created by mijibao on 15/12/30.
+//  Copyright © 2015年 songcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
